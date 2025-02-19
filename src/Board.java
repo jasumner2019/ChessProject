28d0e7646ed11abf789bf6ca53b10634fb3334aa
ashton-sumner-sp <110562301+ashton-sumner-sp@users.noreply.github.com>
2025-02-19 15:38:27 -0700

    Initializing Board Logic

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,20 @@
+import java.util.HashMap;
+
 public class Board {
+    private HashMap<String, Integer> board = new HashMap<>();
+    private String columnNames = "ABCDEFGH";
+    private String rows = "12345678";
+
+    public Board() {
+        // Constructor
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < columnNames.length(); i++) {
+            board.put(String.valueOf(columnNames.charAt(i)), Integer.valueOf(String.valueOf(rows.charAt(i))));
+        }
+    }
+
+    public
 }
