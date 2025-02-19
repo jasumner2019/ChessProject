d44bd7f5b2826371b175fa172f4bcb623f927fb1
ashton-sumner-sp <110562301+ashton-sumner-sp@users.noreply.github.com>
2025-02-19 16:00:05 -0700

    added printBoard

--- src/Board.java
+++ src/Board.java
@@ -16,5 +16,15 @@ public class Board {
         }
     }
 
-    public
+    public printBoard() {
+        System.out.println("H " + "|  ".repeat(8) )
+        System.out.println("G " + "|  ".repeat(8))
+        System.out.println("F " + "|  ".repeat(8))
+        System.out.println("E " + "|  ".repeat(8))
+        System.out.println("D " + "|  ".repeat(8))
+        System.out.println("C " + "|  ".repeat(8))
+        System.out.println("B " + "|  ".repeat(8))
+        System.out.println("A " + "|  ".repeat(8))
+        System.out.println("**" + "|  ".repeat(8))
+    }
 }
